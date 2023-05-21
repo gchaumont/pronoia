5726daa388987bb28cc8a3acde3d3c808e8b0d29
Guy <gchaumont@me.com>
2023-05-21 12:24:05 +0200

    Filter

--- resources/js/filters/RadioFilter.vue
+++ resources/js/filters/RadioFilter.vue
@@ -3,10 +3,15 @@
         <RadioGroup as="li" v-model="radioval">
             <!-- <RadioGroupLabel>Plan</RadioGroupLabel> -->
             <div class="flex flex-col gap-2">
-                <RadioGroupOption v-for="(option, optionIdx) in search.results.facets[props.facet]?.options" :key="option.value" v-slot="{ checked }" :value="option.value" @click="$emit('chosen', option.value)">
-                    <div :class="[checked ? 'bg-white/80 dark:bg-indigo-500/70' : 'hover:bg-white/60 dark:hover:bg-indigo-500/40', 'flex items-between cursor-pointer text-sm rounded-lg  p-2 ']">
-                        <p :class="[checked ? 'dark:text-slate-100':'dark:text-slate-200']">{{option.label || option.value}}</p>
-                        <p class=" rounded  ml-auto font-semibold text-black dark:text-slate-200 " v-if="option.count">{{option.count}}</p>
+                <RadioGroupOption v-for="(option, optionIdx) in searchInstance.results.facets[props.facet]?.options"
+                    :key="option.value" v-slot="{ checked }" :value="option.value" @click="$emit('chosen', option.value)">
+                    <div
+                        :class="[checked ? 'bg-white/80 dark:bg-indigo-500/70' : 'hover:bg-white/60 dark:hover:bg-indigo-500/40', 'flex items-between cursor-pointer text-sm rounded-lg  p-2 ']">
+                        <p :class="[checked ? 'dark:text-slate-100' : 'dark:text-slate-200']">{{ option.label ||
+                            option.value }}
+                        </p>
+                        <p class=" rounded  ml-auto font-semibold text-black dark:text-slate-200 " v-if="option.count">
+                            {{ option.count }}</p>
                     </div>
                 </RadioGroupOption>
             </div>
@@ -22,22 +27,32 @@ import {
     RadioGroupOption,
 } from '@headlessui/vue'
 
-const search = inject('resourceSearch')
+
 
 const props = defineProps({
     facet: {
         type: String,
         required: true
+    },
+    instance: {
+        type: [String, Object],
+        required: false,
+        default: 'resourceSearch'
     }
 })
 
-const radioval = ref(window.App.locale.country)
+const searchInstance = typeof props.instance === 'string' ? inject(props.instance) : props.instance;
 
-// watch(() => search.state?.facetRefinements?.[props.facet],
-//     (newVal) => radioval.value = newVal, {immediate: true})
+const radioval = ref('')
+
+
+watch(() => searchInstance.state?.facetsRefinements?.[props.facet],
+    (newVal) => radioval.value = newVal,
+    { immediate: true }
+)
 
 watch(radioval,
-    () => search.addFacetRefinement(props.facet, radioval)
-    .search())
+    () => searchInstance.addFacetRefinement(props.facet, radioval)
+        .search())
 
 </script>
