a076addde2eb5c4755103ffd7ad21879dd058068
Guy <gchaumont@me.com>
2024-03-30 17:07:50 +0100

    Laravel 11

--- resources/js/Searchbox.vue
+++ resources/js/Searchbox.vue
@@ -1,12 +1,15 @@
 <template>
-    <form method="get" @submit.prevent="" @search="performSearch">
+    <form method="get" @submit.prevent="performSearch">
         <div class=" w-full max-w-xs lg:max-w-md">
-            <label for="search-inut" class="sr-only">{{$filters.trans('general.search')}}</label>
+            <label for="search-inut" class="sr-only">{{ $filters.trans('general.search') }}</label>
             <div class="relative text-gray-400  focus-within:text-gray-700">
                 <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                     <MagnifyingGlassIcon class="h-5 w-5" aria-hidden="true" />
                 </div>
-                <input id="search-inut" class="block w-full transition-colors rounded-md border border-transparent bg-white bg-opacity-10 py-2 pl-10 pr-3 leading-5   placeholder-gray-400 focus:border-transparent focus:bg-opacity-100 focus:text-gray-900 focus:placeholder-gray-500 focus:outline-none focus:ring-0 sm:text-sm" placeholder="Search" type="search" name="search" v-model="search.state.query" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" enterKeyHint="search" autofocus/>
+                <input id="search-inut"
+                    class="block w-full transition-colors rounded-md border border-transparent bg-white bg-opacity-10 py-2 pl-10 pr-3 leading-5   placeholder-gray-400 focus:border-transparent focus:bg-opacity-100 focus:text-gray-900 focus:placeholder-gray-500 focus:outline-none focus:ring-0 sm:text-sm"
+                    placeholder="Search" type="search" name="search" v-model="search.state.query" autocomplete="off"
+                    autocapitalize="off" autocorrect="off" spellcheck="false" enterKeyHint="search" autofocus />
                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pl-3">
                     <LoadingSpinner v-if="search.isLoading" class="h-6 w-6" aria-hidden="true" />
                 </div>
